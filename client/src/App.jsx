6bca7851dc652fad4cdd782f8efe201a171dea61
tumpudikarthikeya <tumpudibhanu@gmail.com>
2024-07-06 13:53:23 +0530

    installed react and tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='text-red-500'>Hi</p>
+    </>
+  )
+}
+
+export default App
