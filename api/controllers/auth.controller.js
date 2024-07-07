4101498e9a6cf15efaf9d65d14d8e0a72c1615cb
tumpudikarthikeya <tumpudibhanu@gmail.com>
2024-07-07 08:15:22 +0530

    added a middleware and finc to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,13 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-const signup = async (req,res) => {
+
+const signup = async (req,res,next) => {
     const {username, email, password} = req.body;
 
     if(!username || !email || !password || username==="" || email ==="" || password==="") {
-        return res.status(400).json({message : "All fields are required"})
+        next(errorHandler(400,"All fields are required"))
     }
     const hashPassword = bcryptjs.hashSync(password,9)
     const newUser = new User({
@@ -17,10 +19,10 @@ const signup = async (req,res) => {
     try {
 
         await newUser.save();
-        return res.json({message:"user ctreated successfully"});
+        return res.json("Signup Successful");
     }
     catch(error) {
-        return res.status(500).json({message:error.message})
+        next(error)
     }
 }
 export default signup
